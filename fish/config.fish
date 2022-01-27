4186411bbffb0aff66b588e395ea714ea70d8956
Lakshmi <33117526+Lakshmikanta0013@users.noreply.github.com>
2022-01-27 11:15:31 +0530

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,236 @@
+#  ____ _____
+# |  _ \_   _|  Derek Taylor (DistroTube)
+# | | | || |    http://www.youtube.com/c/DistroTube
+# | |_| || |    http://www.gitlab.com/dwt1/
+# |____/ |_|
+#
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+### EXPORT ###
+set fish_greeting                                 # Supresses fish's intro message
+set TERM "xterm-256color"                         # Sets the terminal type
+set EDITOR "nvim"                                 # $EDITOR use Visual-Code in terminal
+set VISUAL "nvim"                                 # $VISUAL use Visual-Code in GUI mode
+
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal '#8be9fd'
+set fish_color_autosuggestion '#44475a'
+set fish_color_command '#de93f9'   			
+set fish_color_error '#ff5555'
+set fish_color_param #f1fa8c
+
+
+########################################################################
+############### 				FUNCTION				 ###############
+########################################################################
+
+function mkdir-cd
+    mkdir -p -- $argv && cd -- $argv
+end
+
+function move
+    mv -i $argv
+end
+
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+function remove
+    set original_args $argv
+
+    argparse r f -- $argv
+
+    if not set -q _flag_r || set -q _flag_f
+        rm $original_args
+        return
+    end
+
+    function confirm-remove --argument dir
+        set display_dir (echo $dir | unexpand-home-tilde)
+
+        if confirm "Remove .git directory $display_dir?"
+            rm -rf $dir
+            return
+        end
+
+        echo 'Cancelling.'
+        return 1
+    end
+
+    for f in $argv
+        set gitdirs (find $f -name .git)
+        for gitdir in $gitdirs
+            confirm-remove $gitdir
+        end
+    end
+
+    rm $original_args
+end
+
+function confirm
+    read -P "$argv> " response
+    contains $response y Y yes YES
+end
+
+function clean-unzip --argument zipfile
+    if not test (echo $zipfile | string sub --start=-4) = .zip
+        echo (status function): argument must be a zipfile
+        return 1
+    end
+
+    if is-clean-zip $zipfile
+        unzip $zipfile
+    else
+        set zipname (echo $zipfile | trim-right '.zip')
+        mkdir $zipname || return 1
+        unzip $zipfile -d $zipname
+    end
+end
+
+function unzip-cd --argument zipfile
+    clean-unzip $zipfile && cd (echo $zipfile | trim-right .zip)
+end
+
+########################################################################
+############### 				ALIASES					 ###############
+########################################################################
+
+
+#vim
+alias vim="nvim"
+alias svim="sudo nvim"
+
+#Make a directory with parent-child
+alias mkdir="mkdir -p"
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# Changing "ls" to "exa"
+alias ls='exa -alh --color=always --group-directories-first' # listing
+alias la='exa -ah --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -lh --color=always --group-directories-first'  # long format
+alias lt='exa -aTh --color=always --group-directories-first' # tree listing
+alias l.='exa -ah | egrep "^\."'
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+#pacman command 
+alias pacins='sudo pacman -S'
+alias pacrmv='sudo pacman -Rns'
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'  			# remove orphaned packages
+alias parurmv='paru -Rns'
+alias pacup='sudo pacman -Syu'
+alias yup='yay -Syu'
+alias pup='paru -Syu'
+alias yin='yay -S'
+alias pin='paru -S'
+
+#wifi
+alias wifi="nmtui"
+
+#chmod
+alias mod="sudo chmod +x"
+
+# git
+alias addup='git add -u'
+alias addall='git add .'
+alias branch='git branch'
+alias checkout='git checkout'
+alias clone='git clone'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull origin'
+alias push='git push origin'
+alias tag='git tag'
+alias newtag='git tag -a'
+
+# script to arch system maintaincence
+#To download this https://github.com/voider755/almh.git
+# arch user "yay -S almh-git"
+alias maintaincence='almh.py'
+
+#continue download
+alias wget="wget -c"
+
+#userlist
+alias userlist="cut -d: -f1 /etc/passwd"
+
+#grub update
+alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+
+
+#get fastest mirrors in your neighborhood
+# Download reflector "sudo pacman -S reflector"
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose  --sort rate --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 30 --number 10 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 30 --number 10 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 30 --number 10 --sort age --save /etc/pacman.d/mirrorlist"
+
+#Recent Installed Packages
+# Download expac "sudo pacman -S expac"
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -20 | nl"
+alias riplong="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -3000 | nl"
+
+#get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+#Edit with your EDITOR for important configuration files
+alias nlightdm="sudo $EDITOR /etc/lightdm/lightdm.conf"
+alias npacman="sudo $EDITOR /etc/pacman.conf"
+alias ngrub="sudo $EDITOR /etc/default/grub"
+alias nconfgrub="sudo $EDITOR /boot/grub/grub.cfg"
+alias nmkinitcpio="sudo $EDITOR /etc/mkinitcpio.conf"
+alias nmirrorlist="sudo $EDITOR /etc/pacman.d/mirrorlist"
+alias narcomirrorlist='sudo nano /etc/pacman.d/arcolinux-mirrorlist'
+alias nsddm="sudo $EDITOR /etc/sddm.conf"
+alias nfstab="sudo $EDITOR /etc/fstab"
+alias nbash="$EDITOR ~/.bashrc"
+alias nzsh="$EDITOR ~/.zshrc"
+alias nfish="$EDITOR ~/.config/fish/config.fish"
+
+#systeminfo
+alias probe="sudo -E hw-probe -all -upload"
+alias sysfailed="systemctl list-units --failed"
+
+#shutdown or reboot
+alias shutdown="sudo shutdown now"
+alias reboot="sudo reboot"
+
+#give the list of all installed desktops - xsessions desktops
+alias xd="ls /usr/share/xsessions"
+
+# switch between shells
+# I do not recommend switching default SHELL from bash.
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+# the terminal rickroll
+alias rr='curl -s -L http://bit.ly/10hA8iC | bash'
+
+
+### RANDOM COLOR SCRIPT ###
+# Get this script from my GitLab: gitlab.com/dwt1/shell-color-scripts
+# Or install it from the Arch User Repository: shell-color-scripts
+colorscript random
+
+
+end
+
+starship init fish | source
